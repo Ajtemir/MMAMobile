d8205dd49c8f519b4047cdda452fe3b006f9a531
aulanbekuulu <aulanbekuulu@srs.lan>
2023-11-01 15:48:44 +0600

    wrapped with bloc

--- lib/model/auction/auction_bloc/auction_state.dart
+++ lib/model/auction/auction_bloc/auction_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:upai_app/model/auction/auction_detail_model.dart';
 
 abstract class BaseAuctionState extends Equatable{
@@ -8,12 +9,31 @@ abstract class BaseAuctionState extends Equatable{
 
 abstract class BaseDetailAuctionState extends BaseAuctionState{
   final AuctionDetailModel detail;
+  Widget build();
+
 
   BaseDetailAuctionState(this.detail);
   @override
   List<Object?> get props => [detail];
 }
 
+class AuctionInitial extends BaseDetailAuctionState {
+  AuctionInitial(AuctionDetailModel detail) : super(detail);
+
+  @override
+  Widget build() {
+    return Column(
+      children: [
+        Text(this.runtimeType.toString()),
+        Text(detail.startDate.toString()),
+        Text(detail.endDate.toString()),
+        Text(detail.currentMaxPrice.toString()),
+        Text(detail.startPrice.toString()),
+      ],
+    );
+  }
+
+}
 class SellerProductNotAuctioned extends BaseAuctionState {
 }
 
