f59fce30f69ed12ccfce534dd5f8197ababb64c5
aulanbekuulu <aulanbekuulu@srs.lan>
2023-11-16 14:13:08 +0600

    refactored oop principles

--- lib/model/auction/auction_bloc/auction_state.dart
+++ lib/model/auction/auction_bloc/auction_state.dart
@@ -1,12 +1,22 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:upai_app/model/auction/auction_detail_model.dart';
 
+import '../../../shared/app_colors.dart';
+import '../../../views/category/aboutMagaz.dart';
 import '../auction_state.dart';
+import 'Events/base_auction_event.dart';
+import 'auction_bloc.dart';
 
-abstract class BaseAuctionState extends Equatable{
+abstract class BaseBuildState extends Equatable {
   @override
   List<Object?> get props => [];
+  Widget build(BuildContext context);
+}
+abstract class BaseAuctionState extends BaseBuildState{
 }
 
 abstract class BaseDetailAuctionState extends BaseAuctionState{
@@ -22,31 +32,252 @@ class AuctionInitialState extends BaseDetailAuctionState {
   final AuctionState state;
   AuctionInitialState(AuctionDetailModel detail, this.state) : super(detail);
 
+  @override
+  Widget build(BuildContext context) {
+      // TODO: implement build
+      throw UnimplementedError();
+  }
+
 
 
 }
 class SellerProductNotAuctionedState extends BaseAuctionState {
+  @override
+  Widget build(context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor:
+              MaterialStatePropertyAll(AppColors.red1),
+              padding: const MaterialStatePropertyAll(
+                  EdgeInsets.symmetric(vertical: 10)),
+            ),
+            onPressed: () {
+              showMaterialModalBottomSheet(
+                context: context,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20)),
+                ),
+                builder: (contextInner) =>
+                    SingleChildScrollView(
+                      controller: ModalScrollController.of(
+                          contextInner),
+                      child: MakeAuctionedForm()
+                          .getWidget(context),
+                    ),
+              );
+            },
+            child: Text(
+              "Запустить аукцион",
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
 
 class SellerProductAuctionedState extends BaseDetailAuctionState {
   SellerProductAuctionedState(AuctionDetailModel detail) : super(detail);
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      detail.widget,
+      detail.currentMaxPrice == null
+          ? const SizedBox()
+          : SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor:
+            MaterialStatePropertyAll(AppColors.red1),
+            padding: const MaterialStatePropertyAll(
+                EdgeInsets.symmetric(vertical: 10)),
+          ),
+          onPressed: () {
+            BlocProvider.of<AuctionBloc>(context).add(AuctionSubmitEvent());
+          },
+          child: Text(
+            "Подтвердить аукцион",
+          ),
+        ),
+      ),
+      SizedBox(height: 20,),
+      SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(
+                true ? AppColors.red1 : AppColors.grey),
+            padding: const MaterialStatePropertyAll(
+                EdgeInsets.symmetric(vertical: 10)),
+          ),
+          onPressed: (){
+            BlocProvider.of<AuctionBloc>(context).add(AuctionUnmadeEvent());
+          },
+          child: Text(
+            "Отменить аукцион",
+          ),
+        ),
+      ),
+    ],);
+  }
+
 }
 
 class BuyerAuctionAppliedState extends BaseDetailAuctionState {
   BuyerAuctionAppliedState(AuctionDetailModel detail) : super(detail);
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormBuilderState>();
+    final _bloc = BlocProvider.of<AuctionBloc>(context);
+    var state = this;
+    return FormBuilder(
+      key: _formKey,
+      child: Column(
+        children: [
+          detail.widget,
+          SizedBox(
+            height: 20,
+          ),
+          FormBuilderTextField(
+            name: 'suggestedPrice',
+            validator: (valueCandidate) {
+              if (valueCandidate?.isEmpty ?? true) {
+                return 'This field is required.';
+              }
+              if(state.detail.currentMaxPrice != null && state.detail.currentMaxPrice! > double.parse(valueCandidate!)){
+                return 'Suggested price must be more than current max price ${state.detail.currentMaxPrice}';
+              }
+              else if(double.parse(valueCandidate!) < state.detail.startPrice){
+                return 'Suggested price must be more than start price ${state.detail.startPrice}';
+              }
+              return null;
+            },
+            enabled: true,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+                labelText: 'Предложенная цена', border: OutlineInputBorder()),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(AppColors.red1),
+                padding: const MaterialStatePropertyAll(
+                    EdgeInsets.symmetric(vertical: 10)),
+              ),
+              onPressed: () {
+                if (_formKey.currentState!.saveAndValidate()) {
+                  Map<String, dynamic> keyValuePairs = _formKey.currentState!.value;
+                  var suggestedPrice = double.parse(keyValuePairs['suggestedPrice']);
+                  _bloc.add(AuctionAppliedEvent(suggestedPrice));
+                }
+
+              },
+              child: Text(
+                "Повысить заявку на аукцион",
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class BuyerAuctionNotAppliedState extends BaseDetailAuctionState {
   BuyerAuctionNotAppliedState(AuctionDetailModel detail) : super(detail);
+
+  @override
+  Widget build(BuildContext context) {
+    final _bloc = BlocProvider.of<AuctionBloc>(context);
+    final state = this;
+    final _formKey = GlobalKey<FormBuilderState>();
+    return FormBuilder(
+      key: _formKey,
+      child: Column(
+        children: [
+          state.detail.widget,
+          SizedBox(
+            height: 20,
+          ),
+          FormBuilderTextField(
+            name: 'suggestedPrice',
+            validator: (valueCandidate) {
+              if (valueCandidate?.isEmpty ?? true) {
+                return 'This field is required.';
+              }
+              if(double.parse(valueCandidate!) < state.detail.startPrice){
+                return 'Suggested price must be more than start price';
+              }
+              return null;
+            },
+            enabled: true,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+                labelText: 'Предложенная цена', border: OutlineInputBorder()),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(AppColors.red1),
+                padding: const MaterialStatePropertyAll(
+                    EdgeInsets.symmetric(vertical: 10)),
+              ),
+              onPressed: () {
+                if (_formKey.currentState!.saveAndValidate()) {
+                  Map<String, dynamic> keyValuePairs = _formKey.currentState!.value;
+                  var suggestedPrice = double.parse(keyValuePairs['suggestedPrice']);
+                  _bloc.add(AuctionAppliedEvent(suggestedPrice));
+                }
+
+              },
+              child: Text(
+                "Подать заявку на аукцион",
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class BuyerProductNotAuctionedState extends BaseAuctionState {}
+class BuyerProductNotAuctionedState extends BaseAuctionState {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
 
-class AuctionLoadingState extends BaseAuctionState {}
+class AuctionLoadingState extends BaseAuctionState {
+  @override
+  Widget build(BuildContext context) {
+    return const CircularProgressIndicator();
+  }
+}
 
 class AuctionErrorState extends BaseAuctionState {
   final String errorMessage;
 
   AuctionErrorState(this.errorMessage);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(errorMessage);
+  }
 }
--- lib/model/auction/auction_detail_model.dart
+++ lib/model/auction/auction_detail_model.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/app_colors.dart';
+import '../../widgets/date_format.dart';
+
 class AuctionDetailModel{
     final DateTime startDate;
     final DateTime endDate;
@@ -13,6 +16,115 @@ class AuctionDetailModel{
     Text(endDate.toString()),
     Text(currentMaxPrice.toString()),
     Text(startPrice.toString())];
+
+  Widget get widget => Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.transparent),
+          boxShadow: const [
+            BoxShadow(
+              color: AppColors.grey,
+              offset: Offset(12, 10),
+              blurRadius: 30,
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Icon(
+              Icons.add_business_rounded,
+              color: Colors.white,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Начало',
+                        style: styleTitleInCard,
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        dateFormat(startDate),
+                        style: styleSubtitleInCard,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Конец',
+                        style: styleTitleInCard,
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        dateFormat(endDate),
+                        style: styleSubtitleInCard,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    'Начальная цена товара:',
+                    style: styleTitleInCard,
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    '${startPrice} сом',
+                    style: styleSubtitleInCard,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    'Нынешняя максимальная цена:',
+                    style: styleTitleInCard,
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    currentMaxPrice == null
+                        ? "Нет покупателей"
+                        : '${currentMaxPrice} сом',
+                    style: styleSubtitleInCard,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      );
+
+  TextStyle styleTitleInCard =
+    const TextStyle(color: AppColors.black, fontSize: 16);
+
+    TextStyle styleSubtitleInCard =
+    const TextStyle(color: AppColors.red1, fontSize: 14);
 
   AuctionDetailModel.fromJson(Map<String, dynamic> json) :
       endDate =DateTime.parse(json['endDate']),
